185826b426c6b463c719aed83259aa64761394de
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2023-11-29 10:25:18 +0100

    before adding database

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+//body-parser = middleware for express that analyzes
+//the body of client's http request, it can handle json and urlencoded data
 app.use(bodyParser.urlencoded({ extended: true }));
+//express static = middleware that makes static files from specified directory
+//available to clients
 app.use(express.static("public"));
 
-const tasks = [];
+let tasks = [];
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { tasks });
 });
 
 app.post("/addTask", (req, res) => {
-  let newTask = req.body.task;
+  let newTask = req.body.task; //.task is name of input
+
   tasks.push(newTask);
+
   res.redirect("/");
 });
 
